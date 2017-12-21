9019f0298cd5328ba3bba5a4c6701970e48ab025
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2017-12-21 12:49:45 -0500

    Changes
    -Added a key decryption function
    -Restricted generated keys to alphanumeric values.

--- encryptor.py
+++ encryptor.py
@@ -1,10 +1,10 @@
-import string
-import random
+import random, string
 from itertools import cycle
 
 def set_key():
     global key
-    key = "".join(random.sample((set(string.printable)),64))
+    key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
+    return key
 
 def encrypt(strg):
     global key
@@ -34,3 +34,18 @@ def decrypt(string):
         except:
             continue
     return "".join(map(chr,l))
+
+def decrypt_key(encrypted,decrypted):
+    if len(encrypted) < 64:
+        print("You must use a string longer than 64 characters to decrypt the whole key.")
+        return
+    else:
+        g,l = cycle(iter(encrypted)),[]
+        for v in decrypted:
+            val = next(g)
+            char = ord(v) - ord(val) 
+            if char > 0:
+                char -= 126
+            char = abs(char)
+            l.append(char)
+        return "".join(map(chr,l))
