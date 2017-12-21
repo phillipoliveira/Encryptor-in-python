7982e8ba9d6314bd217192f59badf0624ab0d902
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2017-12-20 20:51:10 -0500

    Added key generator
    Added key generator

---
+++ encryptor.py
@@ -0,0 +1,37 @@
+import string
+import random
+from itertools import cycle
+
+def set_key():
+    global key
+    key = "".join(random.sample((set(string.printable)),64))
+
+def encrypt(strg):
+    global key
+    printable = set(string.printable)
+    strg = filter(lambda x: x in printable, strg)
+    g,l = cycle(iter(key)),[]
+    for i in strg:
+        val = next(g)
+        try:
+            crypt = ord(i) + ord(val)
+            if crypt > 126:
+                crypt -= 126
+            l.append(crypt)
+        except:
+            continue
+    return "".join(map(chr,l))
+
+def decrypt(string):
+    global key
+    g,l = cycle(iter(key)),[]
+    for i in string:
+        val = next(g)
+        try:
+            crypt = ord(i) - ord(val)
+            if crypt < 0:
+                crypt += 126
+            l.append(crypt)
+        except:
+            continue
+    return "".join(map(chr,l))
