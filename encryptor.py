3f9d9e983afd32867a9129c08a29812a3e15244e
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2017-12-21 16:17:41 -0500

    secret and public keys
    broken still

--- encryptor.py
+++ encryptor.py
@@ -1,51 +1,56 @@
 import random, string
+from string import printable
 from itertools import cycle
 
 def set_key():
-    global key
-    key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
-    return key
+    global public_key, private_key
+    public_key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
+    private_key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(64))
+    return public_key, private_key
 
 def encrypt(strg):
-    global key
+    global public_key, private_key
     strg = filter(lambda x: x in (set(string.printable)), strg)
-    g,l = cycle(iter(key)),[]
+    k1,k2,l = cycle(iter(public_key)),cycle(iter(private_key)),[]
     for i in strg:
-        val = next(g)
+        val1,val2 = next(k1),next(k2)
+        try: 
+            crypt = ord(i) + ord(val1) - ord(val2)
+            while crypt > 126:
+                crypt -= 126
+            while crypt < 33:
+                crypt += 126
+            print(crypt)
+            l.append(crypt)
+        except:
+            continue
+    return "".join(map(chr,l))
+
+def decrypt(strg):
+    global public_key, private_key
+    k1,k2,l = cycle(iter(public_key)),cycle(iter(private_key)),[]
+    for i in strg:
+        val1,val2 = next(k1),next(k2)
         try:
-            crypt = ord(i) + ord(val)
-            if crypt > 126:
+            crypt = ord(i) - ord(val1) + ord(val2)
+            while crypt < 33:
+                crypt += 126
+            while crypt > 126:
                 crypt -= 126
             l.append(crypt)
         except:
             continue
     return "".join(map(chr,l))
 
-def decrypt(string):
-    global key
-    g,l = cycle(iter(key)),[]
-    for i in string:
-        val = next(g)
-        try:
-            crypt = ord(i) - ord(val)
-            if crypt < 0:
-                crypt += 126
-            l.append(crypt)
-        except:
-            continue
-    return "".join(map(chr,l))
-
-def decrypt_key(encrypted,decrypted):
-    if len(encrypted) < 64:
-        print("You must use a string longer than 64 characters to decrypt the whole key.")
-        return
-    else:
-        g,l = cycle(iter(encrypted)),[]
+def decrypt__secret_key(encrypted,decrypted,public_key):
         for v in decrypted:
-            val = next(g)
-            char = ord(v) - ord(val) 
-            if char > 0:
+            l = []
+            char = (ord(v) 
+                    + ord(next(cycle(iter(public_key))))
+                    - ord(next(cycle(iter(encrypted))))
+                    )
+            while char < 33:
+                char += 126
+            while char > 126:
                 char -= 126
-            char = abs(char)
-            l.append(char)
-        return "".join(map(chr,l))
+            print(chr(char))
